a15d838e09ec4bd77d02953cab697957818639f7
Abishek Senthil Kumar <abishekkumar2004@gmail.com>
2024-08-01 13:16:36 +0530

    api

--- Netflix_clone-main/script2.js
+++ Netflix_clone-main/script2.js
@@ -1,18 +1,16 @@
 // api key  from TMDB
 
-const api = "api_key=0d7fcb538472b4a248392fdaf9ae8532";
+const api = process.env.API_KEY;
 
 // base url of the site
-const base_url = "https://api.themoviedb.org/3";
+const base_url = 'https://api.themoviedb.org/3';
 
-const banner_url = "https://image.tmdb.org/t/p/original";
-const img_url = "https://image.tmdb.org/t/p/w300"; // You can change "w185" to your preferred size
-
+const banner_url = 'https://image.tmdb.org/t/p/original';
+const img_url = 'https://image.tmdb.org/t/p/w300'; // You can change "w185" to your preferred size
 
 // requests for movies data
 
 const requests = {
-
   fetchTrending: `${base_url}/trending/all/week?${api}&language=en-US`,
   fetchNetflixOrignals: `${base_url}/discover/tv?${api}&with_networks=213`,
   fetchActionMovies: `${base_url}/discover/movie?${api}&with_genres=28`,
@@ -24,32 +22,25 @@ const requests = {
 // used to truncate the string
 
 function truncate(str, n) {
-  return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  return str?.length > n ? str.substr(0, n - 1) + '...' : str;
 }
 // banner
 
 fetch(requests.fetchNetflixOrignals)
   .then((res) => res.json())
 
-
-
   .then((data) => {
-    
     // every refresh the movie will be change
-
 
     const setMovie =
       data.results[Math.floor(Math.random() * data.results.length - 1)];
 
-   
-
-    var banner = document.getElementById("banner");
-    var banner_title = document.getElementById("banner__title");
-    var banner__desc = document.getElementById("banner__description");
-
+    var banner = document.getElementById('banner');
+    var banner_title = document.getElementById('banner__title');
+    var banner__desc = document.getElementById('banner__description');
 
     banner.style.backgroundImage =
-      "url(" + banner_url + setMovie.backdrop_path + ")";
+      'url(' + banner_url + setMovie.backdrop_path + ')';
     banner__desc.innerText = truncate(setMovie.overview, 150);
     banner_title.innerText = setMovie.name;
   });
@@ -58,38 +49,31 @@ fetch(requests.fetchNetflixOrignals)
 fetch(requests.fetchNetflixOrignals)
   .then((res) => res.json())
 
-
   .then((data) => {
-    const headrow = document.getElementById("headrow");
-    const row = document.createElement("div");
+    const headrow = document.getElementById('headrow');
+    const row = document.createElement('div');
 
-    row.className = "row";
-    row.classList.add("netflixrow");
+    row.className = 'row';
+    row.classList.add('netflixrow');
 
     headrow.appendChild(row);
 
+    const title = document.createElement('h2');
 
-    const title = document.createElement("h2");
-
-
-    title.className = "row__title";
-    title.innerText = "NETFLIX ORIGINALS";
-
+    title.className = 'row__title';
+    title.innerText = 'NETFLIX ORIGINALS';
 
     row.appendChild(title);
 
-    const row_posters = document.createElement("div");
-    row_posters.className = "row__posters";
+    const row_posters = document.createElement('div');
+    row_posters.className = 'row__posters';
     row.appendChild(row_posters);
 
-
-
     data.results.forEach((movie) => {
+      const poster = document.createElement('img');
+      poster.className = 'row__posterLarge';
 
-      const poster = document.createElement("img");
-      poster.className = "row__posterLarge";
-
-      var s = movie.name.replace(/\s+/g, "");
+      var s = movie.name.replace(/\s+/g, '');
 
       poster.id = s;
       poster.src = img_url + movie.poster_path;
@@ -103,30 +87,25 @@ fetch(requests.fetchNetflixOrignals)
 fetch(requests.fetchTrending)
   .then((res) => res.json())
 
-
   .then((data) => {
-    const headrow = document.getElementById("headrow");
-    const row = document.createElement("div");
-    row.className = "row";
+    const headrow = document.getElementById('headrow');
+    const row = document.createElement('div');
+    row.className = 'row';
 
     headrow.appendChild(row);
 
-
-    const title = document.createElement("h2");
-    title.className = "row__title";
-    title.innerText = "Top Rated";
+    const title = document.createElement('h2');
+    title.className = 'row__title';
+    title.innerText = 'Top Rated';
     row.appendChild(title);
 
-
-    const row_posters = document.createElement("div");
-    row_posters.className = "row__posters";
+    const row_posters = document.createElement('div');
+    row_posters.className = 'row__posters';
     row.appendChild(row_posters);
-    
-    
+
     data.results.forEach((movie) => {
-      
-      const poster = document.createElement("img");
-      poster.className = "row__posterLarge";
+      const poster = document.createElement('img');
+      poster.className = 'row__posterLarge';
       var s2 = movie.id;
 
       poster.id = s2;
@@ -139,29 +118,24 @@ fetch(requests.fetchTrending)
 fetch(requests.fetchActionMovies)
   .then((res) => res.json())
 
-
   .then((data) => {
-    const headrow = document.getElementById("headrow");
-    const row = document.createElement("div");
-    row.className = "row";
+    const headrow = document.getElementById('headrow');
+    const row = document.createElement('div');
+    row.className = 'row';
     headrow.appendChild(row);
 
-
-    const title = document.createElement("h2");
-    title.className = "row__title";
-    title.innerText = "Action Movies";
+    const title = document.createElement('h2');
+    title.className = 'row__title';
+    title.innerText = 'Action Movies';
     row.appendChild(title);
 
-
-    const row_posters = document.createElement("div");
-    row_posters.className = "row__posters";
+    const row_posters = document.createElement('div');
+    row_posters.className = 'row__posters';
     row.appendChild(row_posters);
 
-    
     data.results.forEach((movie) => {
-    
-      const poster = document.createElement("img");
-      poster.className = "row__poster";
+      const poster = document.createElement('img');
+      poster.className = 'row__poster';
       var s2 = movie.id;
       poster.id = s2;
       poster.src = img_url + movie.backdrop_path;
@@ -173,27 +147,24 @@ fetch(requests.fetchComedyMovies)
   .then((res) => res.json())
 
   .then((data) => {
-    const headrow = document.getElementById("headrow");
-    const row = document.createElement("div");
-    row.className = "row";
+    const headrow = document.getElementById('headrow');
+    const row = document.createElement('div');
+    row.className = 'row';
     headrow.appendChild(row);
 
-
-    const title = document.createElement("h2");
-    title.className = "row__title";
-    title.innerText = "Comedy Movies";
+    const title = document.createElement('h2');
+    title.className = 'row__title';
+    title.innerText = 'Comedy Movies';
     row.appendChild(title);
 
-
-    const row_posters = document.createElement("div");
-    row_posters.className = "row__posters";
+    const row_posters = document.createElement('div');
+    row_posters.className = 'row__posters';
     row.appendChild(row_posters);
-
 
     data.results.forEach((movie) => {
       console.log(movie);
-      const poster = document.createElement("img");
-      poster.className = "row__poster";
+      const poster = document.createElement('img');
+      poster.className = 'row__poster';
       var s2 = movie.id;
       poster.id = s2;
       poster.src = img_url + movie.backdrop_path;
@@ -205,24 +176,24 @@ fetch(requests.fetchHorrorMovies)
   .then((res) => res.json())
 
   .then((data) => {
-    const headrow = document.getElementById("headrow");
-    const row = document.createElement("div");
-    row.className = "row";
+    const headrow = document.getElementById('headrow');
+    const row = document.createElement('div');
+    row.className = 'row';
     headrow.appendChild(row);
 
-    const title = document.createElement("h2");
-    title.className = "row__title";
-    title.innerText = "Horror Movies";
+    const title = document.createElement('h2');
+    title.className = 'row__title';
+    title.innerText = 'Horror Movies';
     row.appendChild(title);
 
-    const row_posters = document.createElement("div");
-    row_posters.className = "row__posters";
+    const row_posters = document.createElement('div');
+    row_posters.className = 'row__posters';
     row.appendChild(row_posters);
 
     data.results.forEach((movie) => {
       console.log(movie);
-      const poster = document.createElement("img");
-      poster.className = "row__poster";
+      const poster = document.createElement('img');
+      poster.className = 'row__poster';
       var s2 = movie.id;
       poster.id = s2;
       poster.src = img_url + movie.backdrop_path;
@@ -234,24 +205,24 @@ fetch(requests.fetchRomanceMovies)
   .then((res) => res.json())
 
   .then((data) => {
-    const headrow = document.getElementById("headrow");
-    const row = document.createElement("div");
-    row.className = "row";
+    const headrow = document.getElementById('headrow');
+    const row = document.createElement('div');
+    row.className = 'row';
     headrow.appendChild(row);
 
-    const title = document.createElement("h2");
-    title.className = "row__title";
-    title.innerText = "Romance Movies";
+    const title = document.createElement('h2');
+    title.className = 'row__title';
+    title.innerText = 'Romance Movies';
     row.appendChild(title);
 
-    const row_posters = document.createElement("div");
-    row_posters.className = "row__posters";
+    const row_posters = document.createElement('div');
+    row_posters.className = 'row__posters';
     row.appendChild(row_posters);
 
     data.results.forEach((movie) => {
       console.log(movie);
-      const poster = document.createElement("img");
-      poster.className = "row__poster";
+      const poster = document.createElement('img');
+      poster.className = 'row__poster';
       var s2 = movie.id;
       poster.id = s2;
       poster.src = img_url + movie.backdrop_path;
@@ -263,25 +234,24 @@ fetch(requests.fetchDocumentaries)
   .then((res) => res.json())
 
   .then((data) => {
-    const headrow = document.getElementById("headrow");
-    const row = document.createElement("div");
-    row.className = "row";
+    const headrow = document.getElementById('headrow');
+    const row = document.createElement('div');
+    row.className = 'row';
 
     headrow.appendChild(row);
-    const title = document.createElement("h2");
-    title.className = "row__title";
-    title.innerText = "Documentaries"
-    ;
+    const title = document.createElement('h2');
+    title.className = 'row__title';
+    title.innerText = 'Documentaries';
     row.appendChild(title);
 
-    const row_posters = document.createElement("div");
-    row_posters.className = "row__posters";
+    const row_posters = document.createElement('div');
+    row_posters.className = 'row__posters';
     row.appendChild(row_posters);
-    
+
     data.results.forEach((movie) => {
       console.log(movie);
-      const poster = document.createElement("img");
-      poster.className = "row__poster";
+      const poster = document.createElement('img');
+      poster.className = 'row__poster';
       var s2 = movie.id;
       poster.id = s2;
       poster.src = img_url + movie.backdrop_path;
